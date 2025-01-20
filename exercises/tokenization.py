c5e6e7adad1de3217014d283be82540c5793d340
Robin Newhouse <robinnewhouse@gmail.com>
2025-01-20 13:41:52 -0800

    Tokenizer class

--- exercises/tokenization.py
+++ exercises/tokenization.py
@@ -1,9 +1,19 @@
 import os
+import re
 
-# Get the directory containing this file
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(CURRENT_DIR, "../samples/the-verdict.txt"), "r", encoding="utf-8") as f:
-    raw_text = f.read()
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        text = re.sub(r'\s+([,.?!"()\'])', r"\1", text)
+        return text
