cca8120be7ccbd844de0d3d2e76bb8517ce9d7f3
Robin Newhouse <robinnewhouse@gmail.com>
2025-02-09 13:49:51 -0800

    Self attention class v1

--- exercises/ch3/self_attention.py
+++ exercises/ch3/self_attention.py
@@ -21,7 +21,7 @@ W_query = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
 W_key = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
 W_value = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
 
-print()
+print("W_query")
 print(W_query)
 # print(W_key)
 # print(W_value)
@@ -30,6 +30,7 @@ query_2 = x_2 @ W_query
 key_2 = x_2 @ W_key
 value_2 = x_2 @ W_value
 
+print("query_2")
 print(query_2)
 # print(key_2)
 # print(value_2)
@@ -41,3 +42,25 @@ print("values.shape:", values.shape)
 
 print(keys)
 print(values)
+
+# compute the attention score omega_22
+keys_2 = keys[1]
+attn_score_22 = query_2.dot(keys_2)
+print("attn_score_22")
+print(attn_score_22)
+
+attn_scores_2 = query_2 @ keys.T
+print("attn_scores_2")
+print(attn_scores_2)
+
+# compute the attention weights
+# scale the attention scores by sqrt(d_k)
+d_k = keys.shape[-1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**.5, dim=-1)
+print("attn_weights_2")
+print(attn_weights_2)
+print(attn_weights_2.sum())
+
+context_vec_2 = attn_weights_2 @ values
+print("context_vec_2")
+print(context_vec_2)
